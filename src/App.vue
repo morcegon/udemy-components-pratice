f397b9385f2dc003ad17e34461b2c09d87f113a9
Renan Andrade <eu@renanandrade.com.br>
2017-12-19 22:17:00 -0200

    task complete

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <div class="row">
+      <app-serverList></app-serverList>
+      <app-serverDetails></app-serverDetails>      
+    </div>
+    <hr>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+  import Header from "./Header.vue";    
+  import ServerList from "./ServerList.vue";
+  import ServerDetails from "./ServerDetails.vue";
+  import Footer from "./Footer.vue";
+
+  export default {
+    components: {
+      'app-header': Header,
+      'app-serverList': ServerList,
+      'app-serverDetails': ServerDetails,
+      'app-footer': Footer
+    }
+  }
+</script>
+
+<style>
+
+</style>
